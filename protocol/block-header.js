288671ed3fdc57708e1a086c7f81b964074b1d96
adrian <94568a@gmail.com>
2021-10-04 12:56:46 -0700

    fix typo, add test block

--- protocol/block-header.js
+++ protocol/block-header.js
@@ -7,9 +7,9 @@ const serialize = (block) => {
     builder.putInt32LE(block.version);
     builder.putBuffer(block.prevBlockHash);
     builder.putBuffer(block.merkleRoot);
-    builder.putUint32LE(block.timestamp);
-    builder.putUint32LE(block.targetBits);
-    builder.putUint32LE(block.nonce);
+    builder.putUInt32LE(block.timestamp);
+    builder.putUInt32LE(block.targetBits);
+    builder.putUInt32LE(block.nonce);
 
     return builder.build();
 
@@ -23,9 +23,9 @@ const deserialize = (obj) => {
         version: reader.readInt32LE(),
         prevBlockHash: reader.readBuffer(32),
         merkleRoot: reader.readBuffer(32),
-        timestamp: reader.readUint32LE(),
-        targetBits: reader.readUint32LE(),
-        nonce: reader.readUint32LE()
+        timestamp: reader.readUInt32LE(),
+        targetBits: reader.readUInt32LE(),
+        nonce: reader.readUInt32LE()
     };
 
 };
